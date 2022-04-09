70571c1b89fc4b7a82ad21db9e11c5c2c550395d
Pedro Ribeiro <pedro.alex.ribeiro@hotmail.com>
2022-04-09 01:18:57 +0100

    Add favourites and followers to project

--- db/migrate/20220408234738_create_followers.rb
+++ db/migrate/20220408234738_create_followers.rb
@@ -3,8 +3,8 @@
 class CreateFollowers < ActiveRecord::Migration[7.0]
   def change
     create_table :followers do |t|
-      t.references :donor, null: false, foreign_key: true
-      t.references :donor, null: false, foreign_key: true
+      t.references :follower, null: false, index: true, foreign_key: { to_table: :donors }
+      t.references :followee, null: false, index: true, foreign_key: { to_table: :donors }
 
       t.timestamps
     end
